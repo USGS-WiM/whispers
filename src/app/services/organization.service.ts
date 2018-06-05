65a95d03ff0d16abb06074e149bcb52c3c35a244
Draper <bdraper@usgs.gov>
2018-06-05 16:52:56 -0500

    advanced event submission form: added 'apply to all location' functionality

---
+++ src/app/services/organization.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import { Subject } from 'rxjs/Subject';
+
+import { APP_SETTINGS } from '@app/app.settings';
+import { APP_UTILITIES } from '@app/app.utilities';
+
+
+import { Organization } from '@interfaces/organization';
+
+@Injectable()
+export class OrganizationService {
+
+  constructor(private _http: Http) { }
+
+  public getOrganizations(): Observable<Organization[]> {
+
+    const options = new RequestOptions({
+      headers: APP_SETTINGS.MIN_AUTH_JSON_HEADERS
+    });
+
+    return this._http.get(APP_SETTINGS.ORGANIZATIONS_URL, options)
+      .map((response: Response) => <Organization[]>response.json())
+      // .do(data => console.log('Samples data: ' + JSON.stringify(data)))
+      .catch(this.handleError);
+  }
+
+  private handleError(error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
+
+}

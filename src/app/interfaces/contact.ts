db4b5590b171c4633dc21448510cceeece4b36d5
Draper <bdraper@usgs.gov>
2018-06-18 14:30:23 -0500

    added funtioanlity ot event submit form

--- src/app/interfaces/contact.ts
+++ src/app/interfaces/contact.ts
@@ -1,9 +1,9 @@
 import { History } from '@interfaces/history';
 export interface Contact extends History {
-    id: number;
-    first_name: string;
-    last_name: string;
-    phone_number: string;
+    id?: number;
+    first_name?: string;
+    last_name?: string;
+    phone_number?: string;
     email_address: string;
     title: string;
     position: string;

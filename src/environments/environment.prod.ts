41e1d7dc24a97ab16077735862d0687d0756ef7a
Draper <bdraper@usgs.gov>
2018-12-20 16:41:23 -0600

    correcting small oversight with env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,8 @@ export const environment = {
   hmr: false,
   version: require('../../package.json').version,
   api_root: 'https://whispersbeta.wim.usgs.gov/test/whispersservices/',
-  banner_content: 'Welcome to WHISPers 2.0. This website is a LIVE BETA site and contains REAL DATA.',
-  banner_text_color: '#40b244'
+  // banner_content: 'Welcome to WHISPers 2.0. This website is a LIVE BETA site and contains REAL DATA.',
+  banner_content: 'This website is under development and CONTAINS FICTICIOUS DATA. ',
+  // banner_text_color: '#40b244',
+  banner_text_color: '#FFFF00'
 };

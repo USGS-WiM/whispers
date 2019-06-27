414e901ec8f2ed8d06506570d158a2ba09565ec9
Draper <bdraper@usgs.gov>
2019-06-27 09:28:38 -0500

    dist build with proper test config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   hmr: false,
   version: require('../../package.json').version,
-  // api_root: 'https://whispersbeta.wim.usgs.gov/test/whispersservices/',
-  api_root: 'https://whispersbeta.wim.usgs.gov/whispersservices/',
-  banner_content: 'Welcome to WHISPers 2.0. This website is a LIVE BETA site and contains REAL DATA.',
-  // banner_content: 'This website is under development and CONTAINS FICTICIOUS DATA. ',
-  banner_text_color: '#40b244',
-  // banner_text_color: '#FFFF00'
+  api_root: 'https://whispersbeta.wim.usgs.gov/test/whispersservices/',
+  // api_root: 'https://whispersbeta.wim.usgs.gov/whispersservices/',
+  // banner_content: 'Welcome to WHISPers 2.0. This website is a LIVE BETA site and contains REAL DATA.',
+  banner_content: 'This website is under development and CONTAINS FICTICIOUS DATA. ',
+  // banner_text_color: '#40b244',
+  banner_text_color: '#FFFF00'
 };

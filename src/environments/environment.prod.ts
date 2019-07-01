4676f8d989228a5f39b5f6f15e2ef7b5180fe908
Draper <bdraper@usgs.gov>
2019-07-01 10:05:11 -0500

    new clean dist build for master, banner removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,22 @@ export const environment = {
   production: true,
   hmr: false,
   version: require('../../package.json').version,
-  api_root: 'https://whisperstest.wim.usgs.gov/whispersservices/',
-  // api_root: 'https://whispers.wim.usgs.gov/whispersservices/',
+
+  ////////////// PROD /////////////////
+  // API root for prod
+  api_root: 'https://whispers.wim.usgs.gov/whispersservices/',
+  // banner for prod
   // banner_content: 'Welcome to WHISPers 2.0. This website is a LIVE BETA site and contains REAL DATA.',
-  banner_content: 'This website is under development and CONTAINS FICTICIOUS DATA. ',
+  // banner color for prod
   // banner_text_color: '#40b244',
-  banner_text_color: '#FFFF00'
+
+  ////////////// TEST /////////////////
+  // banner for test
+  // banner_content: 'This website is under development and CONTAINS FICTICIOUS DATA. ',
+  // banner color for test
+  // banner_text_color: '#FFFF00'
+  // API root for test
+  // api_root: 'https://whisperstest.wim.usgs.gov/whispersservices/',
+
+
 };

d6bbd729eaf8c98727d2dd4a8d521d633e8cf406
lprivette <lprivette@usgs.gov>
2019-10-23 17:24:31 -0400

    added expansion panel with notification settings to notification tab; added user notificaiton table to tab; fixed accent color issue

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Inject } from '@angular/core';
-
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { MatDialog, MatDialogRef } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
 import { MAT_DIALOG_DATA } from '@angular/material';
 
 import { CustomNotificationComponent } from '@app/custom-notification/custom-notification.component';
@@ -13,6 +14,13 @@ export interface Triggers {
   symbol: string;
 }
 
+export interface Notification {
+  notification: string;
+  event_id: number;
+  created_date: string;
+  source: string;
+}
+
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
@@ -20,6 +28,10 @@ export interface Triggers {
 })
 export class NotificationsComponent implements OnInit {
 
+  notificationsDataSource: MatTableDataSource<Notification>;
+  panelOpenState = false;
+  selection;
+  notificationsLoading = false;
   /* Toggle hints for 'Owned and Collaborating Events Notifications' */
   toggle1;
   toggle2;
@@ -41,14 +53,58 @@ export class NotificationsComponent implements OnInit {
     {position: 10, name: 'Event with ID 675465', weight: 20.1797, symbol: 'Ne'},
   ];
 
+  dummyNotifications: Notification[] = [
+    {notification: 'Mark Adams has added a species to Event 170666.', event_id: 170666, created_date: '9/3/2019', source: 'Mark Adams'},
+    {notification: 'Barb Smith has added a diagnosis to event 170131.', event_id: 170131, created_date: '9/3/2019', source: 'Custom Trigger'},
+    {notification: 'An event with E.coli in Minnesota has been added: Event 170676.', event_id: 170676, created_date: '9/3/2019', source: 'System'},
+    {notification: 'Jane Farmington (a member of your organization) has added an Event: Event 170773.', event_id: 170773, created_date: '9/3/2019', source: 'Barb Smith'},
+    {notification: 'An event with White-tailed deer in Minnesota or Wisconsin with the diagnosis Chronic wasting disease has been added: Event 170220.', event_id: 170220, created_date: '9/3/2019', source: 'Custom Trigger'},
+  ];
+
+  notificationDisplayedColumns = [
+    'select',
+    'notification',
+    'created_date',
+    'source'
+  ];
+
+  @ViewChild(MatPaginator) notificationPaginator: MatPaginator;
+
+
   constructor(
     private dialog: MatDialog
   ) { }
 
   ngOnInit() {
+    const initialSelection = [];
+    const allowMultiSelect = true;
+    this.selection = new SelectionModel<Notification>(allowMultiSelect, initialSelection);
+
+    this.notificationsDataSource = new MatTableDataSource(this.dummyNotifications);
+    this.notificationsLoading = true;
+    this.notificationsDataSource.paginator = this.notificationPaginator;
+  }
+
+  navigateToEvent() {
+
   }
 
   newCustomNotification() {
     this.customNotificationRef = this.dialog.open(CustomNotificationComponent);
+  }
+
+  // From angular material table sample on material api reference site
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.notificationsDataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.notificationsDataSource.data.forEach(row => this.selection.select(row));
   }
 }

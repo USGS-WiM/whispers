825aa55b84193d19e303392515b21f145d08dc2d
Estes <njestes@usgs.gov>
2019-12-05 16:46:24 -0800

    add generic compontent for generating event summary report. Added 'generate event summary report' button that does nothing

---
+++ src/app/event-summary-report/event-summary-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-summary-report',
+  templateUrl: './event-summary-report.component.html',
+  styleUrls: ['./event-summary-report.component.scss']
+})
+export class EventSummaryReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

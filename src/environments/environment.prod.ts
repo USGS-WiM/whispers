82d246524eb7877ee280de0b7462437759c74ca1
Draper <bdraper@usgs.gov>
2019-12-13 11:00:04 -0600

    master dist build v2.9.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
 
   ////////////// PROD /////////////////
   // API root for prod
-  // api_root: 'https://whispers.usgs.gov/whispersservices/',
+  api_root: 'https://whispers.usgs.gov/whispersservices/',
   // banner for prod
   // banner_content: 'Welcome to WHISPers 2.0. This website is a LIVE BETA site and contains REAL DATA.',
   // banner color for prod
@@ -17,7 +17,7 @@ export const environment = {
   // banner color for test
   // banner_text_color: '#FFFF00'
   // API root for test
-  api_root: 'https://whisperstest.wim.usgs.gov/whispersservices/',
+  // api_root: 'https://whisperstest.wim.usgs.gov/whispersservices/',
 
 
 };

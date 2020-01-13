e35b2c3042b29e012fb6014fa874af3453e98b8c
Draper <bdraper@usgs.gov>
2020-01-13 12:20:54 -0600

    #755; updated README with new build instructions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,22 +2,7 @@ export const environment = {
   production: true,
   hmr: false,
   version: require('../../package.json').version,
-
   ////////////// PROD /////////////////
   // API root for prod
-  // api_root: 'https://whispers.usgs.gov/whispersservices/',
-  // banner for prod
-  // banner_content: 'Welcome to WHISPers 2.0. This website is a LIVE BETA site and contains REAL DATA.',
-  // banner color for prod
-  // banner_text_color: '#40b244',
-
-  ////////////// TEST /////////////////
-  // banner for test
-  // banner_content: 'This website is under development and CONTAINS FICTICIOUS DATA. ',
-  // banner color for test
-  // banner_text_color: '#FFFF00'
-  // API root for test
-  api_root: 'https://whisperstest.wim.usgs.gov/whispersservices/',
-
-
+  api_root: 'https://whispers.usgs.gov/whispersservices/',
 };

b47d4bfd791d64ce4ba065a33be41fa658cba4e0
Draper <bdraper@usgs.gov>
2020-02-26 15:44:18 -0600

    deletes dist to avoid merge conflicts

--- dist/out-tsc/src/app/interfaces/bulk-upload-record.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=bulk-upload-record.js.map
--- dist/out-tsc/src/environments/environment.hmr.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    production: false,
-    hmr: true,
-    version: require('../../package.json').version
-};
-//# sourceMappingURL=environment.hmr.js.map

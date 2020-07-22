c1e14b78cf2297def56360e61c9d14a9588cf497
Marcus Christie <machrist@iu.edu>
2020-07-22 17:42:14 -0400

    Add warning when your tries to login but email not yet verified

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -15,6 +15,7 @@ import { MatSnackBar } from '@angular/material';
 import { MAT_DIALOG_DATA } from '@angular/material';
 
 import { User } from '@interfaces/user';
+import { ConfirmComponent } from '@app/confirm/confirm.component';
 
 
 @Component({
@@ -32,6 +33,8 @@ export class AuthenticationComponent implements OnInit {
 
   submitLoading = false;
 
+  invalidPassword = false;
+
   constructor(
     formBuilder: FormBuilder,
     public authenticationService: AuthenticationService,
@@ -40,6 +43,7 @@ export class AuthenticationComponent implements OnInit {
     public router: Router,
     private route: ActivatedRoute,
     public snackBar: MatSnackBar,
+    private dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.loginForm = formBuilder.group({
@@ -86,7 +90,21 @@ export class AuthenticationComponent implements OnInit {
           this.submitLoading = false;
           this.authenticationErrorFlag = true;
           if (error.status === 403) {
-            this.openSnackBar('Invalid username and/or password. Please try again.', 'OK', 8000);
+            const errorData = error.json();
+            if (errorData.type === 'unverified_email') {
+              this.dialog.open(ConfirmComponent, {
+                data: {
+                  title: "User Email Not Verified",
+                  titleIcon: "warning",
+                  message:
+                    "Please verify your email address by clicking the link in the email we sent. Once your email is verified you will be able to log in.",
+                  confirmButtonText: "OK",
+                  showCancelButton: false,
+                },
+              });
+            } else {
+              this.openSnackBar('Invalid username and/or password. Please try again.', 'OK', 8000);
+            }
           } else {
             this.openSnackBar('Error. Failed to login. Error message: ' + error, 'OK', 8000);
           }

3dd7c46d4c0d67884c2608f36ce83cd1b6da8e51
Marcus Christie <machrist@iu.edu>
2021-01-20 17:51:15 -0500

    Add reCAPTCHA API key for prod and test envs

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -8,6 +8,6 @@ export const environment = {
   // banner color for test
   // banner_text_color: '#FFFF00'
   // API root for test
-  recaptcha_site_key: '',
+  recaptcha_site_key: '6Lc43c4ZAAAAAO3zFzGMumcVHiPN6vChVPvJqc9N',
   api_root: 'https://whisperstest.wim.usgs.gov/api/',
 };

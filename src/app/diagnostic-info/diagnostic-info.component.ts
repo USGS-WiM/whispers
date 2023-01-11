fd518af32e5f627e2c08950164f5c85aab66aa22
blakedraper <bdraper@usgs.gov>
2023-01-11 16:19:30 -0600

    implements #1317

--- src/app/diagnostic-info/diagnostic-info.component.ts
+++ src/app/diagnostic-info/diagnostic-info.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog, MatDialogRef } from "@angular/material";
+import { APP_SETTINGS } from "@app/app.settings";
 
 @Component({
-  selector: 'app-diagnostic-info',
-  templateUrl: './diagnostic-info.component.html',
-  styleUrls: ['./diagnostic-info.component.scss']
+  selector: "app-diagnostic-info",
+  templateUrl: "./diagnostic-info.component.html",
+  styleUrls: ["./diagnostic-info.component.scss"],
 })
 export class DiagnosticInfoComponent implements OnInit {
+  constructor(
+    public diagnosticInfoDialogRef: MatDialogRef<DiagnosticInfoComponent>
+  ) {}
 
-  constructor(public diagnosticInfoDialogRef: MatDialogRef<DiagnosticInfoComponent>) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  openDiagnosticSubmissionGuideLink() {
+    window.open(
+      APP_SETTINGS.WHISPERS_DIAGNOSTIC_SUBMISSION_GUIDELINES_URL,
+      "_blank"
+    );
   }
-
 }

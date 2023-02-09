204a79a7d1dac22ba694823260888627f5ff37df
blakedraper <bdraper@usgs.gov>
2023-02-09 15:40:57 -0600

    FIX: changes method of display for role name

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,16 +1,16 @@
 export interface User {
-    id: number;
-    username: string;
-    first_name: string;
-    last_name: string;
-    email: string;
-    is_superuser?: boolean;
-    is_staff?: boolean;
-    is_active?: boolean;
-    role?: number;
-    organization?: number;
-    circles?: number[];
-    last_login?: string;
-    active_key?: string;
-    user_status?: string;
+  id: number;
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  is_superuser?: boolean;
+  is_staff?: boolean;
+  is_active?: boolean;
+  role_string?: string;
+  organization?: number;
+  circles?: number[];
+  last_login?: string;
+  active_key?: string;
+  user_status?: string;
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,35 +1,36 @@
+import { map } from "rxjs/operators";
+import { Injectable, Output } from "@angular/core";
+import { Observable, BehaviorSubject, of } from "rxjs";
 
-import {map} from 'rxjs/operators';
-import { Injectable, Output } from '@angular/core';
-import { Observable ,  BehaviorSubject, of } from 'rxjs';
-
-import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
-import { Router } from '@angular/router';
-import { APP_SETTINGS } from '../app.settings';
-import { User } from '@interfaces/user';
-import { EventEmitter } from 'events';
-import { CurrentUserService } from '@services/current-user.service';
-import clientStorage from '@app/client-storage';
+import {
+  Http,
+  Response,
+  Headers,
+  RequestOptions,
+  URLSearchParams,
+} from "@angular/http";
+import { Router } from "@angular/router";
+import { APP_SETTINGS } from "../app.settings";
+import { User } from "@interfaces/user";
+import { EventEmitter } from "events";
+import { CurrentUserService } from "@services/current-user.service";
+import clientStorage from "@app/client-storage";
 
 @Injectable()
-
 export class AuthenticationService {
-
   user: User;
 
   constructor(
     private _http: Http,
     private router: Router,
     private currentUserService: CurrentUserService
-  ) {
-
-  }
+  ) {}
 
   login(username: string, password: string) {
     const options = new RequestOptions({
       headers: new Headers({
-        'Authorization': 'Basic ' + btoa(username + ':' + password)
-      })
+        Authorization: "Basic " + btoa(username + ":" + password),
+      }),
     });
 
     const self = this;
@@ -37,21 +38,27 @@ export class AuthenticationService {
       map((res: any) => {
         self.user = res.json();
         // if (self.user.is_staff || self.user.username == 'testuser') {
-        clientStorage.setItem('username', username);
-        clientStorage.setItem('password', password);
-        clientStorage.setItem('first_name', self.user.first_name);
-        clientStorage.setItem('last_name', self.user.last_name);
-        clientStorage.setItem('email', self.user.email);
-        clientStorage.setItem('is_staff', self.user.is_staff.toString());
-        clientStorage.setItem('is_superuser', self.user.is_superuser.toString());
-        clientStorage.setItem('is_active', self.user.is_active.toString());
-        clientStorage.setItem('role', self.user.role.toString());
-        clientStorage.setItem('organization', self.user.organization.toString());
-        clientStorage.setItem('last_login', self.user.last_login);
-        clientStorage.setItem('active_key', self.user.active_key);
-        clientStorage.setItem('user_status', self.user.user_status);
+        clientStorage.setItem("username", username);
+        clientStorage.setItem("password", password);
+        clientStorage.setItem("first_name", self.user.first_name);
+        clientStorage.setItem("last_name", self.user.last_name);
+        clientStorage.setItem("email", self.user.email);
+        clientStorage.setItem("is_staff", self.user.is_staff.toString());
+        clientStorage.setItem(
+          "is_superuser",
+          self.user.is_superuser.toString()
+        );
+        clientStorage.setItem("is_active", self.user.is_active.toString());
+        clientStorage.setItem("role_string", self.user.role_string);
+        clientStorage.setItem(
+          "organization",
+          self.user.organization.toString()
+        );
+        clientStorage.setItem("last_login", self.user.last_login);
+        clientStorage.setItem("active_key", self.user.active_key);
+        clientStorage.setItem("user_status", self.user.user_status);
 
-        clientStorage.setItem('currentUser', JSON.stringify(self.user));
+        clientStorage.setItem("currentUser", JSON.stringify(self.user));
 
         // self.userLoggedIn$.emit(res);
         // this.currentUser.emit(res);
@@ -60,37 +67,31 @@ export class AuthenticationService {
 
         //   alert('This user is not authorized!');
         // }
-      }));
-
+      })
+    );
   }
 
   logout() {
-
     // this.router.navigate(['/login']);
     // this.router.navigateByUrl('login');
     this.user = undefined;
-    this.currentUserService.updateCurrentUser({ 'username': '' });
-    clientStorage.removeItem('username');
-    clientStorage.removeItem('password');
-    clientStorage.removeItem('first_name');
-    clientStorage.removeItem('last_name');
-    clientStorage.removeItem('email');
-    clientStorage.removeItem('is_staff');
-    clientStorage.removeItem('is_superuser');
-    clientStorage.removeItem('is_active');
-    clientStorage.removeItem('role');
-    clientStorage.removeItem('organization');
-    clientStorage.removeItem('last_login');
-    clientStorage.removeItem('active_key');
-    clientStorage.removeItem('user_status');
+    this.currentUserService.updateCurrentUser({ username: "" });
+    clientStorage.removeItem("username");
+    clientStorage.removeItem("password");
+    clientStorage.removeItem("first_name");
+    clientStorage.removeItem("last_name");
+    clientStorage.removeItem("email");
+    clientStorage.removeItem("is_staff");
+    clientStorage.removeItem("is_superuser");
+    clientStorage.removeItem("is_active");
+    clientStorage.removeItem("role");
+    clientStorage.removeItem("organization");
+    clientStorage.removeItem("last_login");
+    clientStorage.removeItem("active_key");
+    clientStorage.removeItem("user_status");
 
-    clientStorage.removeItem('currentUser');
+    clientStorage.removeItem("currentUser");
 
     return of(true);
-
   }
-
-
-
 }
-
